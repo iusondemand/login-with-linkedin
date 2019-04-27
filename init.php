6278bdf50416e21a00953c5affada00e71434f05
Valentino Spataro <spataro@iusseek.eu>
2019-04-27 12:06:38 +0200

    Update init.php
    Just a Global to avoid duplicate definitions.
    Others prefers this method:
    define('const',      "xxxxx");
    The constant will be available also inside the functions. It's more elegant: https://www.php.net/manual/en/function.constant.php
    
    ps: I was writing almost the same :) This really speed up my time, thanks !

--- init.php
+++ init.php
@@ -25,11 +25,7 @@ function curl($url, $parameters)
 
 function getCallback()
 {
-    $client_id = "</*Client Id*/>";
-    $client_secret = "</*Secret key*/>";
-    $redirect_uri = "</*Redirection Url*/>";
-    $csrf_token = rand(1111111, 9999999);
-    $scopes = "r_liteprofile%20r_emailaddress";
+	global 	$client_id ,$client_secret , $redirect_uri , $csrf_token , $scopes;
 
     if (isset($_REQUEST['code'])) {
         $code = $_REQUEST['code'];
